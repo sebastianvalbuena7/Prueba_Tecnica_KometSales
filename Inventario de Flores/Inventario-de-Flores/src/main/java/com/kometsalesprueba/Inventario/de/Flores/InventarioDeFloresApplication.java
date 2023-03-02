6bc713fb73f68a2122d2a26e9cec063828d985ee
Sebastian Valbuena <sebastianvalbuena25@gmail.com>
2023-03-02 09:36:06 -0500

    Dependencias necesarias instaladas

---
+++ Inventario de Flores/Inventario-de-Flores/src/main/java/com/kometsalesprueba/Inventario/de/Flores/InventarioDeFloresApplication.java
@@ -0,0 +1,13 @@
+package com.kometsalesprueba.Inventario.de.Flores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioDeFloresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioDeFloresApplication.class, args);
+	}
+
+}
---
+++ Inventario de Flores/Inventario-de-Flores/src/test/java/com/kometsalesprueba/Inventario/de/Flores/InventarioDeFloresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kometsalesprueba.Inventario.de.Flores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioDeFloresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
